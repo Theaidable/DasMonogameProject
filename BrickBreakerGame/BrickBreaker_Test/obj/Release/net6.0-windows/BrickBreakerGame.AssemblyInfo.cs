eeee434d989bf5eecd7d5e3c3fecd294076d4c29
davi3553 <dada5@student.eadania.dk>
2024-11-06 14:38:38 +0100

    Navneændring
    Har ændret navn væk fra test

--- BrickBreakerGame/BrickBreaker_Test/obj/Release/net6.0-windows/BrickBreakerGame.AssemblyInfo.cs
+++ BrickBreakerGame/BrickBreaker_Test/obj/Release/net6.0-windows/BrickBreakerGame.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("BrickBreaker_Test")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("BrickBreakerGame")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f262c5baade9da3202e1086f7ceaa096033ff59c")]
-[assembly: System.Reflection.AssemblyProductAttribute("BrickBreaker_Test")]
-[assembly: System.Reflection.AssemblyTitleAttribute("BrickBreaker_Test")]
+[assembly: System.Reflection.AssemblyProductAttribute("BrickBreakerGame")]
+[assembly: System.Reflection.AssemblyTitleAttribute("BrickBreakerGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
