8d3e89e702fba602c549cf4046beae5f39fa9933
Miktastik <sike5@student.eadania.dk>
2024-11-14 13:51:50 +0100

    HighScore gemte
    Tilføjede at highscore bliver gemt i AppData\Local\BrickBreakerGame\highscore.txt

--- BrickBreakerGame/obj/Release/net6.0-windows/BrickBreakerGame.AssemblyInfo.cs
+++ BrickBreakerGame/obj/Release/net6.0-windows/BrickBreakerGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BrickBreakerGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1f7a4ac05f142bce9bb2e4ce163f8e6341e5d25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6c8bcda6fce5979491ca84944b9a65ab8e72e93")]
 [assembly: System.Reflection.AssemblyProductAttribute("BrickBreakerGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BrickBreakerGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
